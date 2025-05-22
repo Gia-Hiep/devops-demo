8cbc5e0896c4334652597d65af522f0a8eb6f3d2
Gia-Hiep <hiepprook9@gmail.com>
2025-05-22 22:09:12 +0700

    Refund

--- src/main/java/com/example/devopsdemo/Calculator.java
+++ src/main/java/com/example/devopsdemo/Calculator.java
@@ -2,6 +2,6 @@ package com.example.devopsdemo;
 
 public class Calculator {
     public int add(int a, int b) {
-        return a + b + 1; // Lỗi: trả về sai giá trị
+        return a + b;
     }
 }
